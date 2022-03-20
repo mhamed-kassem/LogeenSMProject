e552743e1a830984a831cbbeebc48420b295f3ea
Taha <tahaamin.eg@gmail.com>
2022-03-20 23:29:37 +0200

    Stock update add update delet select

--- LogeenStockManagement/Controllers/StockController.cs
+++ LogeenStockManagement/Controllers/StockController.cs
@@ -16,7 +16,8 @@ namespace LogeenStockManagement.Controllers
         {
             Context = context;
         }
-        //[Route("stocks")]
+        //[route("stocks")]
+        [HttpGet]
         public IActionResult GetAll()
         {
             List<Stock> stocks = Context.Stocks.ToList();
@@ -24,7 +25,7 @@ namespace LogeenStockManagement.Controllers
         }
 
         [HttpGet("{id}")]
-        //[HttpGet("stocks/{id}")]
+        //[Route("stocks/{id}")]
         public IActionResult GetOne(int id)
         {
             Stock stock = Context.Stocks.FirstOrDefault(s => s.Id == id);
@@ -32,14 +33,36 @@ namespace LogeenStockManagement.Controllers
             return Ok(stock);
         }
 
-        [Route("stocks/Create")]
-        public IActionResult Create(Stock stock)
+        //[Route("stocks/Create")]
+        [HttpPost]
+        public IActionResult Post(Stock stock)
         {
             Context.Stocks.Add(stock);
+            Context.SaveChanges();
             return Ok("Created Successfully");
         }
 
+        [HttpPut("{id}")]
+        //[Route("stocks/update")]
+        public void Put(int id ,Stock value)
+        {
+            var stock = Context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stock != null)
+            {
+                Context.Entry<Stock>(stock).CurrentValues.SetValues(value);
+                Context.SaveChanges();
+            }
+        }
 
-
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var stock = Context.Stocks.FirstOrDefault(s => s.Id == id);
+            if (stock != null)
+            {
+                Context.Stocks.Remove(stock);
+                Context.SaveChanges();
+            }
+        }
     }
 }
